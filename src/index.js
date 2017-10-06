7ad7714a060a65a4fd9635877dc4fcd3c1deece7
Jeff Chung <drifterz303@gmail.com>
2017-10-06 17:44:29 +0900

    add asset setup example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import _ from 'lodash';
+import Block from './style.css';
+import './block.png';
 
 function component(){
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
-    
+    element.classList.add('hello');
+    var myImg = new Image();
+    myImg.src = Block;
+    element.appendChild(myImg);
     return element;
 }
 
