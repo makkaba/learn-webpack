9e9bea3464b2db3fceeb09302add8b6dedbaf1bb
Jeff Chung <drifterz303@gmail.com>
2017-10-09 14:52:43 +0900

    add firebase ui module

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import Firebase from 'firebase';
-
+import Firebase from '../config/firebaseConfig';
 
 class App extends Component{
+    componentDidMount(){
+        Firebase.ui.start('#root', Firebase.uiConfig);
+        Firebase.app.auth().onAuthStateChanged(function(user) {
+            
+            console.log("hello"+user);
+        });
+        
+    }
     render(){
         return (
             <h1>hello</h1>
