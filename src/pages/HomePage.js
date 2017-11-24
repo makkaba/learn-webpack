e6e784093f234f4f5df6d88f49dd0d201f25c132
Jeff Chung <drifterz303@gmail.com>
2017-11-24 17:32:50 +0900

    expose categories on home page.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,50 @@
 import MainLayout from '../containers/MainLayout';
 import React, { Component } from 'react';
 import Firebase from '../config/firebaseConfig';
+import categoryItems from '../mock/categoryItems.json';
+import Card, { CardActions, CardContent } from 'material-ui/Card';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
 
+//TODO: grid card, style card, make it link. 
 class homePage extends Component{
   constructor(props){
     super(props);
+    this.state = {
+        categories: []
+    };
   }
   
   componentDidMount(){
-      
+      fetchCategories();
+  }
+  
+  fetchCategories(){
+      this.setState({categories: categoryItems.data});
   }
   
   render(){
+      // const { categories } = this.state;
     return (
         <MainLayout>
-            <div><h1>home page!</h1></div>
+            <div>
+                <h1>home page!</h1>
+                
+                <div>
+                    {this.state.categories.map((category, index) => {
+                        return (
+                            <Card key={index}>
+                                <CardContent>
+                                    {category.name}
+                                </CardContent>
+                                <CardActions>
+                                    <Button dense>Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        );
+                    })}
+                </div>
+            </div>
         </MainLayout>
       
     );
