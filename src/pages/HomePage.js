70a1914816fc7ccc86bc19d1a8b3f30cb3558a67
Jeff Chung <drifterz303@gmail.com>
2017-11-25 23:42:08 +0900

    add style to category card.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 import Firebase from '../config/firebaseConfig';
 import categoryItems from '../mock/categoryItems.json';
 import Card, { CardActions, CardContent } from 'material-ui/Card';
+import Grid from 'material-ui/Grid';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
+import '../styles/category.scss';
 
 //TODO: grid card, style card, make it link. 
 class homePage extends Component{
@@ -31,18 +33,22 @@ class homePage extends Component{
                 <h1>home page!</h1>
                 
                 <div>
+                    <Grid container spacing={24} className="category-container">
                     {this.state.categories.map((category, index) => {
                         return (
-                            <Card key={index}>
-                                <CardContent>
-                                    {category.name}
-                                </CardContent>
-                                <CardActions>
-                                    <Button dense>Learn More</Button>
-                                </CardActions>
-                            </Card>
+                                <Grid item xs={6}>
+                                    <Card key={index}>
+                                        <CardContent>
+                                            {category.name}
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button dense>Learn More</Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
                         );
                     })}
+                    </Grid>
                 </div>
             </div>
         </MainLayout>
